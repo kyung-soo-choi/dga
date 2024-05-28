dafde4b67b0ffede9695b5056fc26672bdddcfe8
soouniverse <167866041+soouniverse@users.noreply.github.com>
2024-05-29 01:11:38 +0200

    [TEST] Final 1

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ class WeatherData(models.Model):
     Enthält Felder für Zeitstempel, Datum, maximale und minimale Temperatur, Wochentag und Icon.
     """
     timestamp = models.DateTimeField()
+
     date = models.DateField()
     max_temp = models.DecimalField(max_digits=5, decimal_places=2)
     min_temp = models.DecimalField(max_digits=5, decimal_places=2)
